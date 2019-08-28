957285b28490bec86f7ef61004ac7261c9bd5c49
hamid <hamid331994@gmail.com>
2019-08-28 15:05:17 +0430

    dev

--- src/components/layout/Congrats.js
+++ src/components/layout/Congrats.js
@@ -1,8 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import TodoContext from '../../context/todo/todoContext';
 
 const Congrats = () => {
-  const { allDone } = useContext(TodoContext);
+  const { todos } = useContext(TodoContext);
+
+  const [allDone, setAllDone] = useState(false);
+
+  let listOfDone = [];
+  todos.forEach(t => t.done && listOfDone.push(t.done));
+  if(listOfDone.length === todos.length && todos.length !== 0) {
+    setAllDone(true);
+  } else {
+    setAllDone(false);
+  }
 
   return (
     <div className={`container-sm my-0 ${!allDone && 'd-n'}`}>

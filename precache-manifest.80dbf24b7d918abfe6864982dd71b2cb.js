a1b147b9ff22b96c9e71ddc476376fa654945e3f
hamid <hamid331994@gmail.com>
2019-08-31 20:53:01 +0430

    Updates

---
+++ precache-manifest.80dbf24b7d918abfe6864982dd71b2cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b665ccefd4ff33cf3e800723b963de89",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "c1d511a1a8c2bd77b345",
+    "url": "/trello-clone/static/css/main.01fbec13.chunk.css"
+  },
+  {
+    "revision": "7321b4759775a5b4608e",
+    "url": "/trello-clone/static/js/2.4b0f2c21.chunk.js"
+  },
+  {
+    "revision": "c1d511a1a8c2bd77b345",
+    "url": "/trello-clone/static/js/main.a6ad22ab.chunk.js"
+  },
+  {
+    "revision": "a7969dd5f4de691f9b43",
+    "url": "/trello-clone/static/js/runtime~main.559d3c57.js"
+  }
+]);

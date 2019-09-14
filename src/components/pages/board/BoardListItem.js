cc43382b016d7788f7ddb9ed5aef387cc15356e9
hamid <hamid331994@gmail.com>
2019-09-14 15:48:18 +0430

    dev

--- src/components/pages/board/BoardListItem.js
+++ src/components/pages/board/BoardListItem.js
@@ -1,5 +1,4 @@
 import React, { useRef, useContext } from 'react';
-
 import ListItemLabels from './ListItemLabels';
 
 import UserContext from '../../../context/user/userContext';
@@ -17,20 +16,11 @@ const BoardListItem = ({ item }) => {
   }
 
   const onEBtnClick = (e) => {
-    console.log('use useRef here for setFastEditModalPos')
-    if(e.target.classList.contains('func-e-btn')) {
-      setFastEditModalPos({
-        top: e.target.parentElement.getBoundingClientRect().top,
-        left: e.target.parentElement.getBoundingClientRect().left,
-        width: e.target.parentElement.getBoundingClientRect().width
+    setFastEditModalPos({
+        top: listItem.current.getBoundingClientRect().top,
+        left: listItem.current.getBoundingClientRect().left,
+        width: listItem.current.getBoundingClientRect().width
       });
-    } else {
-      setFastEditModalPos({
-        top: e.target.parentElement.parentElement.getBoundingClientRect().top,
-        left: e.target.parentElement.parentElement.getBoundingClientRect().left,
-        width: e.target.parentElement.parentElement.getBoundingClientRect().width
-      });
-    }
     setModal('on', 'fastEditModal');
   }
 
